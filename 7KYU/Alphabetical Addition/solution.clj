1c5a4d58355d157998641b8624b7dd8272b8bbdf
Stuart Stein <qmstuart@gmail.com>
2020-01-20 03:05:26 +0000

    added handy clojure functions

---
+++ 7KYU/Alphabetical Addition/solution.clj
@@ -0,0 +1,12 @@
+(ns letters.addition)
+(def alphabet {\a 1 \b 2 \c 3 \d 4 \e 5 \f 6 \g 7 \h 8 \i 9 \j 10 \k 11 \l 12 \m 13
+               \n 14 \o 15 \p 16 \q 17 \r 18 \s 19 \t 20 \u 21 \v 22 \w 23 \x 24 \y 25 \z 26})
+
+(defn add-letters [letters]
+  (println letters)
+  (cond (empty? letters) \z
+         :else (as-> (map #(alphabet %) letters) v
+                     (reduce + 0 v)
+                     (mod v 26)
+                     (cond (= 0 v) \z
+                              :else (char (+ 96 v))))))
