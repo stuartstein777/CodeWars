269a59b6a1f63160b31fcf0ff47d4e7d25aec94d
Stuart Stein <qmstuart@gmail.com>
2020-01-30 00:52:24 +0000

    Update solution.clj

--- 4KYU/Secret Strings/solution.clj
+++ 4KYU/Secret Strings/solution.clj
@@ -2,13 +2,13 @@
   (:require [clojure.set :only [map-invert]]))
   
 (defn remove-last [c m]
-  (dissoc (reduce-kv (fn [acc k v] (into acc {k (vec (filter #(not= % c) v))})) {} m) c))
+  (dissoc (reduce-kv (fn [acc k v] (into acc {k (filter #(not= % c) v)})) {} m) c))
 
 (defn recover-secret [xs]
   (loop [result ""
          remaining (reduce #(as-> (update %1 (second %2)  conj (first %2)) o
                                   (update o  (nth %2 2)   conj (first %2) (second %2))
                                   (update o  (first %2)   (fnil conj []))) {} xs)]
-    (let [next ((map-invert remaining) [])]
+    (let [next ((set/map-invert remaining) [])]
       (cond (nil? next) result
             :else (recur (str result next) (remove-last next remaining))))))
