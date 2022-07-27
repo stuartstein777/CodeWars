e61f1e8abeed4943863302b6d6f69cc8976600fd
Stuart Stein <qmstuart@gmail.com>
2022-07-27 22:16:15 +0100

    Update EnumerableExtensions.cs

--- Handy Functions/CSharp/EnumerableExtensions.cs
+++ Handy Functions/CSharp/EnumerableExtensions.cs
@@ -13,4 +13,17 @@ public static class Extensions
         }
         return result;
     }  
+    
+    public static Dictionary<T, int> Frequencies<T>(this IEnumerable<T> xs) where T : notnull
+    {
+        var res = new Dictionary<T, int>();
+        foreach (var x in xs)
+        {
+            if (res.ContainsKey(x))
+                res[x]++;
+            else
+                res.Add(x, 1);
+        }
+        return res;
+    }
 }
