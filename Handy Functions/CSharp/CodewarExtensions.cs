daf8609ea23c2d752616211449cbf370e94b884e
Stuart Stein <qmstuart@gmail.com>
2023-04-26 23:22:21 +0100

    added handy function

--- Handy Functions/CSharp/CodewarExtensions.cs
+++ Handy Functions/CSharp/CodewarExtensions.cs
@@ -13,4 +13,16 @@ public static class CodewarExtensions
             _ => "th"
         };
     }
+
+    public static IEnumerable<int> AllIndexesOf(this string str, string searchstring)
+    {
+        var minIndex = str.IndexOf(searchstring);
+        while (minIndex != -1)
+        {
+            yield return minIndex;
+            minIndex = str.IndexOf(searchstring, minIndex + searchstring.Length);
+        }
+    }
+
+    public static string Reverse(this string s) => string.Join("", s.ToCharArray().Reverse());
 }
