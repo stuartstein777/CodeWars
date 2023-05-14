5a354d0e4de9172504f4dbc4d00a0f42a917be8b
Stuart Stein <qmstuart@gmail.com>
2023-05-14 20:35:26 +0100

    added solutions

--- lang-scratch/go/main.go
+++ lang-scratch/go/main.go
@@ -1,23 +1,27 @@
 package main
 
-func Fusc(n int) int {
-	total := 0
-	for n != 0 {
-		if n == 1 {
-			total += 1
-		}
+import (
+	"fmt"
+	"strings"
+)
 
-		if n&1 == 0 {
-			n /= 2
-		} else {
-			n = (n - 1) / 2
-		}
-	}
-	return total
-}
+// func Fusc(n int) int {
+// 	total := 0
+// 	for n != 0 {
+// 		if n == 1 {
+// 			total += 1
+// 		}
+
+// 		if n&1 == 0 {
+// 			n /= 2
+// 		} else {
+// 			n = (n - 1) / 2
+// 		}
+// 	}
+// 	return total
+// }
 
 func main() {
-	println(Fusc(10))
+	a := "a"
+	fmt.Println(strings.Repeat(a, 5))
 }
-
-// 1771561
