5ce1a72053f1ad12db42485e9d6ea73f8831d82e
Stuart Stein <qmstuart@gmail.com>
2023-05-17 00:03:39 +0100

    added solutions

--- 5KYU/Conway's Look and Say - Generalized/solution.cs
+++ 5KYU/Conway's Look and Say - Generalized/solution.cs
@@ -3,47 +3,50 @@ using System.Collections.Generic;
 
 public static class Kata
 {
-  public static int[] NumberToDigits(ulong n)
+  private static int[] NumberToDigits(ulong n)
   {
-      var digits = new List<int>();
-      while (n != 0)
-      {
-          digits.Insert(0, (int)(n % 10));
-          n /= 10;
-      }
-      return digits.ToArray();
+    var digits = new List<int>();
+    while (n != 0)
+    {
+        digits.Insert(0, (int)(n % 10));
+        n /= 10;
+    }
+    return digits.ToArray();
   }
-
-  public static string Partition(int[] xs)
+  
+  private static ulong Partition(IReadOnlyList<int> xs)
   {
-     var n = xs[0];
-     var c = 1;
-     var result = "";
-
-     for (var i = 1; i < xs.Length; i++)
-     {
-         var x = xs[i];
-         if (x == n)
-         {
-             c++;
-         }
-         else
-         {
-             result += $"{c}{n}";
-             n = x;
-             c = 1;
-         }
-     }
-
-     return result + $"{c}{n}";
+    var n = xs[0];
+    var c = 1;
+    ulong result = 0;
+    for (var i = 1; i < xs.Count; i++)
+    {
+        var x = xs[i];
+        if (x == n)
+        {
+            c++;
+        }
+        else
+        {
+            if (result != 0)
+                result *= 100;
+            
+            result += (ulong)(c * 10);
+            result += (ulong)n;
+            n = x;
+            c = 1;
+        }
+    }
+    result *= 100;
+    result += (ulong)(c * 10);
+    result += (ulong)n;
+    return result;
   }
   
   public static ulong LookSay(ulong number)
   {
-      if(number == 0) return 10ul;
-    
-      var digits = NumberToDigits(number);
-      var partitions = Partition(digits);
-      return ulong.Parse(partitions);  
+    if(number == 0) return 10ul;
+    var digits = NumberToDigits(number);
+    return Partition(digits);
   }
 }
