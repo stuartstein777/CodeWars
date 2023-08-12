92fb9601514a3141951a452f7922ec194495ba00
Stuart Stein <qmstuart@gmail.com>
2023-08-12 12:45:03 +0100

    added solutions

---
+++ 5KYU/The Road-Kill Detective/solution.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Dinglemouse
+{
+    public class LetterCount
+    {
+        public char Letter { get; set; }
+        public int Count { get; set; }
+    }
+    
+    /*
+        Partition each possibility and animal into parts with a count of the letters. e.g. for
+        rabbit
+        
+        possible animal     = [r, 1] - [a, 1] - [b, 2] - [i, 1] - [t, 1]
+        remains rrraaabbbit = [r, 3] - [a, 3] - [b, 3] - [i, 1] - [t, 1]
+        
+        as long as they have the same number of parts and the letter for each partition is the same at
+        each location, and the remains count is >= naimal partition count, then its a match.
+    
+    */
+    public static List<LetterCount> PartitionByLetter(string s)
+    {
+        List<LetterCount> partitions = new();
+        var current = new LetterCount { Letter = s[0], Count = 1 };
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (s[i] != current.Letter)
+            {
+                partitions.Add(current);
+                current = new LetterCount { Letter = s[i], Count = 1 };
+                
+            }
+            else
+            {
+                current.Count++;
+            }
+        }
+
+        partitions.Add(current);
+        return partitions;
+    }
+    
+    public static string GetAnimal(string remains)
+    {
+        var possibilities = Preloaded.ANIMALS.Where(a => a.StartsWith(remains.Substring(0, 1)));
+
+        foreach (var possibility in possibilities)
+        {
+            var animalParts = PartitionByLetter(possibility);
+            var remainsParts = PartitionByLetter(remains);
+           
+            var found = true;
+            for (var i = 0; i < animalParts.Count; i++)
+            {
+                if (animalParts[i].Letter != remainsParts[i].Letter || animalParts[i].Count > remainsParts[i].Count)
+                {
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found && remainsParts.Count == animalParts.Count)
+            {
+                return possibility;
+            }
+        }
+
+        return null;
+    }
+    
+    public static string RoadKill(string photo)
+    {
+        var pattern = "[^=]";
+
+        var remains = Regex.Matches(photo, pattern);
+
+        if(!remains.Any()) return "??";
+        
+        var result = GetAnimal(string.Join("", remains.Select(m => m.Value)));
+        if (result is null)
+            result = GetAnimal(string.Join("", string.Join("", remains.Reverse().Select(m => m.Value))));
+        return result ?? "??";
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![codewars badge](<https://www.codewars.com/users/qmstuart/badges/large>)
 
-Total Completed Kata:      1267
+Total Completed Kata:      1268
 
 <table border="2" cellspacing="0" cellpadding="6" rules="groups" frame="hsides">
 
@@ -105,16 +105,16 @@ Total Completed Kata:      1267
 <tr>
 <td class="org-left">C#</td>
 <td class="org-left">3 kyu</td>
-<td class="org-right">63.0%</td>
+<td class="org-right">63.8%</td>
 <td class="org-right">266</td>
 <td class="org-right">241</td>
 <td class="org-right">109</td>
-<td class="org-right">23</td>
+<td class="org-right">24</td>
 <td class="org-right">10</td>
 <td class="org-right">3</td>
 <td class="org-right">&#xa0;</td>
 <td class="org-right">&#xa0;</td>
-<td class="org-right">652</td>
+<td class="org-right">653</td>
 </tr>
 
 
